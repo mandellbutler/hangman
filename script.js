b85642a6f05293aa0ae204c08036ae51058467df
Mandell Butler <mandell.butler@outlook.com>
2022-01-21 05:59:30 -0500

    restructured html wireframe

--- script.js
+++ script.js
@@ -6,29 +6,27 @@ const stats = document.getElementById("stats");
 const timer = document.getElementById("timer");
 
 const startButton = document.createElement("button");
-const word = document.createElement("h1");
 const statLabel = document.createElement("h3");
-const timerEl = document.createElement("p");
 
 
 startButton.setAttribute("id", "start-button");
 
 word.setAttribute("id", "word");
-word.textContent = "Displayed Word";
+
 
 statLabel.textContent = "Stat Area";
-timerEl.innerHTML = `Timer: <span>00:45</span>`;
 
 //STARTING DATA
 
 //FUNCTIONS
 
 //USER INTERACTIONS
+    //user presses start game button
+        //timer begins
+        //the black spaces appear in word area
 
 //INITIALIZATIONS
 
 startButton.textContent = "Reset"
 header.append(startButton);
-wordContainer.append(word);
 aside.prepend(statLabel);
-timer.append(timerEl);
