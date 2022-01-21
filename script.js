2bd4e9bf2c9e3c5e1c8dc2d85183257b3b76a512
Mandell Butler <mandell.butler@outlook.com>
2022-01-21 05:44:19 -0500

    hard coded html elements

--- script.js
+++ script.js
@@ -5,12 +5,9 @@ const aside = document.getElementById("game-status");
 const stats = document.getElementById("stats");
 const timer = document.getElementById("timer");
 
-const title = document.createElement("h3");
 const startButton = document.createElement("button");
 const word = document.createElement("h1");
 const statLabel = document.createElement("h3");
-const wins = document.createElement("p");
-const loses = document.createElement("p");
 const timerEl = document.createElement("p");
 
 
@@ -20,9 +17,9 @@ word.setAttribute("id", "word");
 word.textContent = "Displayed Word";
 
 statLabel.textContent = "Stat Area";
-wins.innerHTML = `Wins: <span>0</span>`;
-loses.innerHTML = `Loses: <span>0</span>`;
 timerEl.innerHTML = `Timer: <span>00:45</span>`;
+
+//STARTING DATA
 
 //FUNCTIONS
 
@@ -30,12 +27,8 @@ timerEl.innerHTML = `Timer: <span>00:45</span>`;
 
 //INITIALIZATIONS
 
-title.textContent = "Start Game Area";
 startButton.textContent = "Reset"
-header.append(title);
 header.append(startButton);
 wordContainer.append(word);
 aside.prepend(statLabel);
-stats.append(wins);
-stats.append(loses);
 timer.append(timerEl);
