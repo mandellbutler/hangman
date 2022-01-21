1bd7622373dbf912046bf52e894da3c9cdfa4c59
Mandell Butler <mandell.butler@outlook.com>
2022-01-20 20:07:17 -0500

    finished wireframe

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //DEPENDENCIES
 const header = document.getElementById("start-game");
 const wordContainer = document.getElementById("word-container");
+const aside = document.getElementById("game-status");
 const stats = document.getElementById("stats");
 const timer = document.getElementById("timer");
 
-const title = document.createElement("h1");
+const title = document.createElement("h3");
 const startButton = document.createElement("button");
-const word = document.createElement("span");
+const word = document.createElement("h1");
+const statLabel = document.createElement("h3");
 const wins = document.createElement("p");
 const loses = document.createElement("p");
 const timerEl = document.createElement("p");
@@ -15,8 +17,9 @@ const timerEl = document.createElement("p");
 startButton.setAttribute("id", "start-button");
 
 word.setAttribute("id", "word");
-word.textContent = "Hello";
+word.textContent = "Displayed Word";
 
+statLabel.textContent = "Stat Area";
 wins.innerHTML = `Wins: <span>0</span>`;
 loses.innerHTML = `Loses: <span>0</span>`;
 timerEl.innerHTML = `Timer: <span>00:45</span>`;
@@ -27,11 +30,12 @@ timerEl.innerHTML = `Timer: <span>00:45</span>`;
 
 //INITIALIZATIONS
 
-title.textContent = "Hello World!";
+title.textContent = "Start Game Area";
 startButton.textContent = "Reset"
 header.append(title);
 header.append(startButton);
 wordContainer.append(word);
+aside.prepend(statLabel);
 stats.append(wins);
 stats.append(loses);
 timer.append(timerEl);
