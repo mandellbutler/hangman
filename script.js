45b6e0d486f34ebcaa9a0f74fc441806a3c1f7b8
Mandell Butler <mandell.butler@outlook.com>
2022-01-21 06:05:57 -0500

    activated start game button

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 //DEPENDENCIES
-const header = document.getElementById("start-game");
+const startButton = document.getElementById("start-game");
 const wordContainer = document.getElementById("word-container");
 const aside = document.getElementById("game-status");
 const stats = document.getElementById("stats");
 const timer = document.getElementById("timer");
 
-const startButton = document.createElement("button");
-const statLabel = document.createElement("h3");
 
 
-startButton.setAttribute("id", "start-button");
-
-word.setAttribute("id", "word");
 
 
-statLabel.textContent = "Stat Area";
+
+
+
+
 
 //STARTING DATA
 
 //FUNCTIONS
+function startGame () {
+    //timer begins
+        //the black spaces appear in word area
+    console.log("Let's roll!")
+}
 
 //USER INTERACTIONS
     //user presses start game button
-        //timer begins
-        //the black spaces appear in word area
+    startButton.addEventListener("click", (startGame))
+        
 
 //INITIALIZATIONS
 
-startButton.textContent = "Reset"
-header.append(startButton);
-aside.prepend(statLabel);
+
+
+
