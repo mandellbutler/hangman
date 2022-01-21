02b6852bfa73a8e88e64d475cb00f7ea4a4cfc30
Mandell Butler <mandell.butler@outlook.com>
2022-01-21 06:10:25 -0500

    activated reset game button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //DEPENDENCIES
 const startButton = document.getElementById("start-game");
+const resetButton = document.getElementById("reset-game")
 const wordContainer = document.getElementById("word-container");
 const aside = document.getElementById("game-status");
 const stats = document.getElementById("stats");
@@ -23,11 +24,17 @@ function startGame () {
     console.log("Let's roll!")
 }
 
+function resetGame () {
+    //new word displays with blank spaces
+    //timer resets
+    console.log("Let's bounce!")
+}
 //USER INTERACTIONS
     //user presses start game button
     startButton.addEventListener("click", (startGame))
         
-
+    //user presses reset button
+    resetButton.addEventListener("click", (resetGame))
 //INITIALIZATIONS
 
 
